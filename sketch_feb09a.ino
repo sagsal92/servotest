aeb588c6b3bc643a10b21e160dde3a3eae5c33c2
Sagar salunkhe <sagsal92@gmail.com>
2018-02-09 20:47:09 +0530

    Add files via upload

---
+++ sketch_feb09a.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+Servo myservo;
+int pos = 0;
+String pos_="";
+
+void setup()
+{
+  Serial.begin(9600);
+  myservo.attach(9);
+}
+
+void loop()
+{
+  if (Serial.available()>0) 
+  {
+      pos = Serial.parseInt();
+      myservo.write(pos); 
+      Serial.println(pos);
+      pos=0;
+   }
+}
